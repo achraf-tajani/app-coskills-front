e5d50d69e95f9b588495ea2bd5760605e1bdbfa0
achraf-tajani <achraf.tajani.bis@gmail.com>
2020-09-15 17:15:47 +0200

    feat: init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,78 @@
+import { NgModule } from '@angular/core';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+
+@NgModule({
+  imports: [
+    MatMenuModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBottomSheetModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatListModule,
+    MatTabsModule,
+    MatAutocompleteModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatSidenavModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonToggleModule
+  ],
+  exports: [
+    MatMenuModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBottomSheetModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatListModule,
+    MatTabsModule,
+    MatAutocompleteModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatSidenavModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonToggleModule
+  ]
+})
+export class MaterialModule {}
+
+
